265f520e71d7dc289529b44532233fff1bc02e60
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-04-02 21:35:49 +0530

    Create Solution.java

---
+++ Best Time to Buy and Sell Stock/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for(int price : prices) {
+            if(price < minPrice) {
+                minPrice = price;
+            } else {
+                profit = Math.max(profit, price - minPrice);
+            }
+        }
+        
+
+        return profit;
+    }
+}

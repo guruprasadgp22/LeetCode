9d4e58f33d6767b2c295a0e691e85241e404e766
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-04-03 09:04:35 +0530

    Create Solution.java

---
+++ Single Number 2/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+
+        for(int ele: nums) {
+            ones = (ones ^ ele) & ~twos;
+            twos = (twos ^ ele) & ~ones;
+        }
+
+        return ones;
+    }
+}

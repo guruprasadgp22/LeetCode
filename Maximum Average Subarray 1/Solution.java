d578b89cffa952d25b73c87d88d0c564d5a47859
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-04-03 13:29:41 +0530

    Create Solution.java

---
+++ Maximum Average Subarray 1/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(flowerbed.length == 0) {
+            return false;
+        }
+        
+        int count = 0;
+        if(count == n) {
+            return true;
+        }
+        
+
+        for(int i=0;i<flowerbed.length;i++) {
+            if(flowerbed[i] == 0) {
+                if((i==0 || flowerbed[i-1] == 0) && (i==flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+                if(count == n) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}

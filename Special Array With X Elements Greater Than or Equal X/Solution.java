67b16d89edef34dc0fea44499b97a8c3cf106dc7
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-05-08 12:07:47 +0530

    Create Solution.java

---
+++ Special Array With X Elements Greater Than or Equal X/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        for(int i=0;i<=nums.length;i++) {
+            int count = 0;
+
+            for(int ele: nums) {
+                if(ele >= i) {
+                    count++;
+                }
+            }
+
+            if(count == i) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}

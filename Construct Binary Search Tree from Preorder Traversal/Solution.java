e9eeb32ed8b3a84ac8002fa3b61042912893680d
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-05-09 21:12:15 +0530

    Create Solution.java

---
+++ Construct Binary Search Tree from Preorder Traversal/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return helper(preorder, Integer.MAX_VALUE);
+    }
+
+    int i = 0;
+    public TreeNode helper(int[] preorder, int bound) {
+        if(i >= preorder.length || preorder[i] > bound) {
+            return null;
+        }
+        
+        TreeNode root = new TreeNode(preorder[i]);
+        i++;
+
+        root.left = helper(preorder, root.val);
+        root.right = helper(preorder, bound);
+
+        return root;
+    }
+}

c015ddf8261a89e7d5941df55352c9a91d07472e
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-05-26 10:40:05 +0530

    Create Solution.java

---
+++ Subarray Sums Divisible by K/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int count = 0;
+        int sum = 0;
+
+        for(int i=0;i<nums.length;i++) {
+            sum += nums[i];
+
+            int remainder = (sum % k + k) % k;
+
+            if(map.containsKey(remainder)) {
+                count += map.get(remainder);
+            }
+
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        return count;
+    }
+}

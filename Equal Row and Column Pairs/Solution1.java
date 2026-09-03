7ac07673d1f439752d8b25fc06acf593a4b85e96
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-09-03 14:15:51 +0530

    Create Solution1.java

---
+++ Equal Row and Column Pairs/Solution1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+
+        int result = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                int count = 0;
+                for(int k=0;k<n;k++) {
+                    if(grid[i][k] == grid[k][j]) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+
+                if(count == n) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}

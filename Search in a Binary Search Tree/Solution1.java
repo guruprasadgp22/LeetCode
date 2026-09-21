6c5394231105abac90da7ddd3a93ee9e8b2c607f
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-09-21 17:35:21 +0530

    Create Solution1.java

---
+++ Search in a Binary Search Tree/Solution1.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        return solve(root, val);
+    }
+
+    private TreeNode solve(TreeNode root, int val) {
+        if(root == null) {
+            return null;
+        }
+
+        if(root.val == val) {
+            return root;
+        }
+
+        if(root.val > val) {
+            return solve(root.left, val);
+        } else {
+            return solve(root.right, val);
+        }
+    }
+}

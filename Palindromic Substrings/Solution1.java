54962b70cefae449b97873d61c5cb8d0a822eca2
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-09-23 11:52:25 +0530

    Implement palindrome substring counting solution

---
+++ Palindromic Substrings/Solution1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        int n = s.length();
+
+        for(int i=0;i<n;i++) {
+            for(int j=i;j<n;j++) {
+                if(isPalindrome(i, j, s)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isPalindrome(int start, int end, String s) {
+        while(start <= end) {
+            if(s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}

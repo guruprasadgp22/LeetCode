6f55478c5a72e2d394f13f130399aaee6de35076
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-09-24 17:14:25 +0530

    Create Solution1.java

---
+++ Largest Positive Integer That Exists With Its Negative/Solution1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxK(int[] nums) {
+        int maxEle = -1;
+
+        for(int i=0;i<nums.length;i++) {
+            for(int j=i;j<nums.length;j++) {
+                if(nums[i] == -nums[j] && maxEle < Math.max(nums[i], nums[j])) {
+                    maxEle = Math.max(nums[i], nums[j]);
+                }
+            }
+        }
+
+        return maxEle;
+    }
+}
